1928a72282514875126d04416a5a9a38f29c05d6
Carlos Carrillo <ccarrillo@plainconcepts.com>
2019-01-12 14:11:59 +0100

    Analyze data frame

---
+++ R/AnalyzeDomains.R
@@ -0,0 +1,13 @@
+library(dplyr)
+
+#' Verify the domains analized and verifies if contains malware
+#'
+#' @return  dataframe including if contains malware information
+#' @export
+analizy_domains <- function(df_domains, malware_domain_list)
+{
+  df_domains <- df_domains %>%
+    mutate(contains_malware = domain %in% malware_domain_list)
+
+  return(df_domains)
+}
